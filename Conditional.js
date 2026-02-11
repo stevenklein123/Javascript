0a72c9fda967257ce6300605abee8abcbfb2175d
Steven <fernandezklein123@gmail.com>
2026-02-12 00:29:56 +0800

    Basic conditional Statement

---
+++ Conditional.js
@@ -0,0 +1,62 @@
+//Ternary
+// let studentName = "Steven";
+// let studentScore = 90;
+
+// let result = studentScore > 90 ? "Excellent" : 
+//             studentScore > 75 ? "Good":
+//             studentScore > 60 ? "Pass" :
+//             "Fail"
+
+//             console.log(`${studentName} : ${studentScore}`)
+
+//if
+
+// let temperature = 32;
+
+// if(temperature > 30){
+//     console.log("It's hot.");
+// }
+
+//if else
+
+// let age = 18
+// if(age > 17){
+//     console.log("Adult");
+// }
+// else{
+//     console.log("Minor");
+// }
+
+
+//bool and logical 
+// let age = 20;
+// let hasID = false;
+
+// if(age >= 18 && hasID){
+//     console.log("Can enter club");
+// }else{
+//     console.log("Cannot enter club");
+// }
+
+//switch
+let fruit = "Banana";
+
+
+switch(fruit)
+{
+    case "Apple":
+        console.log("Red");
+        break;
+    case "Banana":
+        console.log("Yellow");
+        break;
+    case "Orange":
+        console.log("Orange");
+        break;
+    case "Grapes":
+        console.log("Purple");
+        break;
+    default:
+        console.log("Unknown");
+    
+}
