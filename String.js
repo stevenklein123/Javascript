df728577b96f058ddee3dfaca0025fa559aaf15c
Steven <fernandezklein123@gmail.com>
2026-02-12 01:07:51 +0800

    String methods,literals,etc.

---
+++ String.js
@@ -0,0 +1,10 @@
+let firstName = "Steven";
+let lastName = "Fernandez";
+
+console.log(`${firstName} ${lastName}`);
+
+let sentece = "Javascript is fun";
+
+console.log(sentece.length);
+console.log(sentece.toUpperCase());
+console.log(sentece.slice(14,18));
