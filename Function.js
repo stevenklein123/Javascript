56e2597c270fd0afe6ac0711c14ea6cbf0be7b8f
Steven <fernandezklein123@gmail.com>
2026-02-12 21:37:14 +0800

    Learning Function

---
+++ Function.js
@@ -0,0 +1,9 @@
+let n = "wew";
+
+function name(name){
+
+    let result = name == "Steven" ? console.log(`Hello ${name} you are Admin.`) : console.log(`Hello ${name} you are User.`);
+
+}
+
+name(n);
