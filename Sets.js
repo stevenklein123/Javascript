f871fcb9c33176920ddec273d5a67efc20771e50
Steven <fernandezklein123@gmail.com>
2026-02-18 01:21:06 +0800

    Sets in JS Unique Value

---
+++ Sets.js
@@ -0,0 +1,68 @@
+// const numbers = new Set();
+
+// numbers.add(1);
+// numbers.add(2);
+// numbers.add(3);
+
+// console.log(numbers);
+
+// numbers.delete(2);
+// console.log(numbers);
+
+// let hasNumber = numbers.has(3) ? "Yes" : "No";
+
+// console.log(hasNumber);
+
+// numbers.clear();
+
+// console.log(numbers);
+
+// console.log(numbers.size);
+
+
+// const arr = [1, 2, 2, 3, 4, 4];
+
+// const unique = [new Set(arr)];
+
+// console.log(unique);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fruits = ["apple", "banana", "apple", "orange", "banana", "kiwi"];
+
+// const numbers = [1,2,3,4,5,5,19];
+
+// const uniqueFruits = [...new Set(fruits)];
+// const uniqueNumbers = [... new Set(numbers)];
+
+// const sortedNumbers = uniqueNumbers.sort((a,b) => b-a);
+
+// const sortedFruits = uniqueFruits.sort();
+
+// console.log(uniqueFruits);
+// console.log(uniqueNumbers);
+
+
+// const letters = new Set(["a", "b", "c"]);
+
+// // For-of loop
+// for (let letter of letters) {
+// }
+
+// // forEach loop
+// letters.forEach(l => console.log(l));
